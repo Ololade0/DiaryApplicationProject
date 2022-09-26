e5c1e29538d57dea2f28113ea9435906e58bcf80
Ololade0 <adesuyiololade@gmail.com>
2022-09-26 12:12:01 +0100

    diary application

---
+++ src/DiaryApplication/Entry.java
@@ -0,0 +1,48 @@
+package DiaryApplication;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+public class Entry {
+    private LocalDateTime dateTime = LocalDateTime.now();
+    private UUID uuidAsString;
+    private String title;
+    private String body;
+
+
+    public Entry(String title, String body){
+        this.uuidAsString = UUID.randomUUID();
+        this.title = title;
+        this.body = body;
+
+    }
+
+    public UUID getId() {
+        return uuidAsString;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+
+    public String getDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return dateTime.format(formatter);
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s%n%s%n", title,body);
+    }
+}
+
---
+++ src/DiaryApplication/User.java
@@ -0,0 +1,66 @@
+package DiaryApplication;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class User {
+    private final String name;
+    private final ArrayList<Diary> myUser;
+    private final String createdDiary;
+    private final String password;
+
+    public User(String name, String createDiary, String password) {
+        this.name = name;
+        this.password = password;
+        myUser = new ArrayList<>();
+        this.createdDiary = createDiary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getcreatediary() {
+        return createdDiary;
+    }
+
+    public void addNewDiary(Diary diary1) {
+
+        myUser.add(diary1);
+    }
+    public Diary ViewDiary(int index) {
+
+        return myUser.get(index);
+    }
+
+    public ArrayList<Diary> getAllViewedDiary() {
+        return myUser;
+    }
+
+
+    public void deleteDiary(String diaryName) {
+        for (int i = 0; i < myUser.size() ; i++) {
+            if(Objects.equals(myUser.get(i).getDiaryName(), diaryName)){
+                myUser.remove(i);
+            }
+
+        }
+    }
+
+    public boolean findDeleteDiary(String diaryName) {
+        // for (Diary diary : myUser) {
+        for (Diary diary : myUser) {
+            if (diary.getDiaryName() == diaryName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean checkPassword(String password) {
+        return Objects.equals(this.password, password);
+    }
+
+}
